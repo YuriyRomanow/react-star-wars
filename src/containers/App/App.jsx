f56fd745a7863899e1906f5efdbbba635aa97874
yura <yuriyromanow123@gmail.com>
2022-10-12 08:26:25 +0300

    0.0.6 - коммит по завершению 6 видео

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -1,12 +1,11 @@
+import PeoplePage from '../PeoplePage';
 
 import styles from './App.module.css';
 
-import { getApiResource } from '../../utils/network';
-
 const App = () => {
-  return (
-      <h1 className={styles.header}>Hello</h1>
-  )
+	return (
+		<PeoplePage />
+	)
 }
 
 export default App;
--- src/utils/network.js
+++ src/utils/network.js
@@ -1,28 +1,24 @@
-const SWAPI_ROOT = 'https://swapi.dev/api/';
-const SWAPI_PEOPLE = 'people';
 
 export const getApiResource = async (url) => {
-
-
     try {
         const res = await fetch(url);
 
         if (!res.ok) {
-            console.error('Could not fetch!', res.status);
+            console.error('Could not fetch.', res.status);
             return false;
         }
-        return await res.json();
+
+        return await res.json(); 
     } catch (error) {
-        console.error('Could not fetch! ', error.message);
+        console.error('Could not fetch.', error.message);
         return false;
     }
 }
 
-// getApiResource(SWAPI_ROOT + SWAPI_PEOPLE)
+// getApiResource(SWAPI_ROOT+SWAPI_PEOPLE)
 //     .then(body => console.log(body))
 
-    (async () => {
-        const body = await getApiResource(SWAPI_ROOT + SWAPI_PEOPLE);
-        console.log(body);
-    })();
-
+// (async () => {
+//     const body = await getApiResource(SWAPI_ROOT+SWAPI_PEOPLE);
+//     console.log(body);
+// })();
