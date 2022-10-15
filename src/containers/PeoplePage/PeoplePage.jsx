ce614b5f6e666bab6f802228cd446637d43a91d9
yura <yuriyromanow123@gmail.com>
2022-10-15 18:32:12 +0300

    0.0.7 - коммит по завершению 9 видео

--- src/containers/PeoplePage/PeoplePage.jsx
+++ src/containers/PeoplePage/PeoplePage.jsx
@@ -1,21 +1,23 @@
-import { useState, useEffect } from 'react';
+import {useState, useEffect} from 'react';
+import PropTypes from 'prop-types';
+
 import {withErrorApi} from "../../hoc-helpers/withErrorApi";
-import { getApiResource } from '../../utils/network';
-import { API_PEOPLE } from '../../constants/api';
-import { getPeopleId, getPeopleImage } from '../../services/getPeopleData';
 import PeopleList from '../../components/PeoplePage/PeopleList';
+import {getApiResource} from '../../utils/network';
+import {getPeopleId, getPeopleImage} from '../../services/getPeopleData';
+import {API_PEOPLE} from '../../constants/api';
 
 import styles from './PeoplePage.module.css';
 
-const PeoplePage = ({ setErrorApi }) => {
+const PeoplePage = ({setErrorApi}) => {
     const [people, setPeople] = useState(null);
 
 
     const getResource = async (url) => {
         const res = await getApiResource(url);
 
-        if(res) {
-            const peopleList = res.results.map(({ name, url }) => {
+        if (res) {
+            const peopleList = res.results.map(({name, url}) => {
                 const id = getPeopleId(url);
                 const img = getPeopleImage(id);
 
@@ -40,9 +42,13 @@ const PeoplePage = ({ setErrorApi }) => {
     return (
         <>
             <h1>Navigation</h1>
-            {people && <PeopleList people={people} />}
+            {people && <PeopleList people={people}/>}
         </>
     )
+}
+
+PeoplePage.propTypes = {
+    setErrorApi: PropTypes.func
 }
 
 export default withErrorApi(PeoplePage);
